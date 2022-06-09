99aa6fa1183005baa50a8dc0e3c2348f63a79f2d
nathangauth88 <nathangauth@gmail.com>
2022-06-08 21:40:48 -0700

    JS updates

---
+++ README.md
@@ -0,0 +1,37 @@
+# Brewery Finder
+
+A simple locator site that allows users to find breweries within a given city 
+
+# User Stories 
+
+- As a user, I want to be able to enter my city into a search bar 
+
+- As a user, I should be able to see a list at the bottom of the page showing the breweries within a city
+
+- As a user, If I enter a new city in the search, that information should replace the old information
+
+- As a user, I should be able to clear my search field 
+
+
+# Technologies Used
+
+- HTML5
+- CSS3
+- Javascript
+- Bootstrap 
+
+# Screenshots
+
+SCREENSHOT OF FINDER
+
+#  Getting Started
+
+[Click here]() to find beer
+
+# Future Enhancements 
+
+- Add ability for users to see where the brewery is on a live map
+
+- Allow user(s), to add/save breweries for future reference
+
+- Have a box/card that shows the results neatly 
--- script.js
+++ script.js
@@ -1,44 +1,57 @@
+// const variables 
+
+
+
+//variables
+
+// let city;
+
+// city.addEventListener('click', (e) => {
+//     city = 'search'
+
+
+//dom elements 
+
 const url = 'https://api.openbrewerydb.org/breweries'
+const search = document.querySelector("#searchBar")
+const cityShow = document.getElementById("cityResult");
 
 
 
-// function getData (event) {    
-//      event.preventDefault()
-// let textInput = document.querySelector('#inputBar').value;
-// console.log(textInput)
-//  ///FETCH code
-   fetch(url) 
-        .then(result => { 
-            return result.json() 
-         })
-        .then (result => {
-            console.log(result)
-        }) 
-       
 
 
-// fetch(url)
-//     .then(response => response.json())
-//     .then(data => {
-//         let pokemon = data.results;
-//         let pokeDiv = document.querySelector('.pokemon');
-//         pokemon.forEach(pokemon => {
-//             let pokemonName = pokemon.name;
-//             let pokemonUrl = pokemon.url;
-//             let pokemonId = pokemonUrl.split('/')[6];
-//             let image = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemonId}.png`;
-//             let div = document.createElement('div');
-//             div.classList.add('pokemon-item');
-//             div.innerHTML = `
-//                 <div class="pokemon-item-image">
-//                     <img src="${image}" alt="${pokemonName}">
-//                 </div>
-//                 <div class="pokemon-item-info">
-//                     <h3>${pokemonName}</h3>
-//                 </div>
-//             `;
-//             pokeDiv.appendChild(div);
-//         });
-//     }).catch(error => {
-//         console.log(error);
-//     })
+
+
+function getData (event) {  
+    event.preventDefault()
+let textInput = document.querySelector("#input").value;
+console.log(textInput)
+
+    fetch(url)
+        .then(res => { 
+            // console.log (res.json())
+            return res.json();
+        })
+        .then(res => {
+           console.log(res.promiseResult[textInput].city)
+           cityShow.innerHTML = res.promiseResult[textInput].city;
+        })
+}
+
+search.addEventListener("submit", getData)
+
+
+    
+
+        
+    // functions 
+
+    // somethingToClick 
+        // when you click search show results by city 
+    //
+    
+    
+    //event listeners 
+
+    //show results when you click search
+    // clear results when you click clear 
